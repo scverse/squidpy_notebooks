a87218ff6d57852f4ea44d5bd3ee260b6338886f
giovp <giov.pll@gmail.com>
2021-01-21 12:04:16 +0100

    mod example

--- examples/graph/interaction_matrix.py
+++ examples/graph/interaction_matrix.py
@@ -1,3 +1,4 @@
+# +
 """
 Compute interaction matrix
 -----------------------
@@ -15,14 +16,15 @@ import squidpy as sq
 adata = sq.datasets.imc()
 adata
 
-# %%
+# +
 # First, we need to compute a connectivity matrix from spatial coordinates.
 # We can use :func:`squidpy.gr.spatial_neighbors` for this purpose.
 
 sq.gr.spatial_neighbors(adata)
 
-# %%
+# +
 # We can compute the interaction matrix with :func:`squidpy.gr.interaction_matrix`
 # and visualize results with :func:`squidpy.pl.interaction_matrix`.
 
-sq.pl.interaction_matrix(adata)
+sq.gr.interaction_matrix(adata, cluster_key="cell type")
+sq.pl.interaction_matrix(adata, cluster_key="cell type")
