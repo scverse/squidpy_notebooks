0653a74b90d65feb409a1302bc8f0020093f8ba4
michalk8 <46717574+michalk8@users.noreply.github.com>
2021-01-31 00:20:48 +0100

    Add support for fixed tutorials (#38)

--- tutorials/graph/tutorial_dummy2.py
+++ tutorials/graph/tutorial_dummy2.py
@@ -1,6 +1,6 @@
 """
-This is a dummy tutorial
-------------------------
+This is a dummy2 tutorial
+-------------------------
 """
 from anndata import AnnData
 import squidpy as sp
@@ -20,7 +20,8 @@ adata
 # %%
 # This is a new cell. We can reference the docs as :func:`squidpy.gr.moran`.
 # Note than any such references or code usage will be automatically linked under that function.
-# See example at :ref:`sphx_glr_auto_examples_graph_compute_dummy.py`.
+# See example at :ref:`sphx_glr_auto_tutorials_image_tutorial_dummy1.py`.
+# Citation - :cite:`celloracle`.
 adata.obsm["spatial"] = np.stack([r.randint(0, 500, 200), r.randint(0, 500, 200)], axis=1)
 sp.gr.spatial_neighbors(adata, spatial_key="spatial", n_rings=2)
 adata
