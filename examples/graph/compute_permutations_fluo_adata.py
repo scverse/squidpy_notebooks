a801c2360d2adef311950a340fd44246f0492431
giovp <giov.pll@gmail.com>
2021-01-31 17:50:30 +0100

    modifiy enrichment example

--- examples/graph/compute_permutations_fluo_adata.py
+++ examples/graph/compute_permutations_fluo_adata.py
@@ -1,35 +1,29 @@
 """
-Neighbors enrichment analysis (Visium Fluo)
+Neighbors enrichment analysis
 ------------------------
 This example shows how to run the neighbors enrichment analysis routine in squidpy.
-It calculates based on pre-defined clusters the proximity between those in the calculated connetivity graph. The number of observed events is compared versus permutations and Z-scores are summarized.
+It calculates based on pre-defined clusters the proximity between those in the calculated connectivity graph.
+The number of observed events is compared versus permutations and Z-scores are summarized.
 """
-
-###############################################################################
-# To get started, we import squidpy
 
 import squidpy as sq
 
-###############################################################################
-# Load a dataset of interest
-
 adata = sq.datasets.visium_fluo_adata()
-
-###############################################################################
-# Calculate the neighbors graph and the enrichment counts and Z-scores
-
-sq.gr.spatial_neighbors(adata)
-sq.gr.nhood_enrichment(adata, cluster_key='leiden')
-
-""
-# The results are stored in two matrices: One for z-scores and one for counts
-# ['leiden_nhood_enrichment']
 adata
 
-""
-# Pairs 6 and 8 and are significantly co-enriched
-sq.pl.nhood_enrichment(adata, cluster_key='leiden', cmap='Blues',
-                       cbar_kws={'label': 'Z-score'}, figsize=[4, 4], vmin=0, vmax=5)
+# %%
+# This dataset contains cell type annotations in adata.obs
+# which are used for calculation of the neigborhood enrichment.
+# First, we need to compute a connectivity matrix from spatial coordinates.
 
-""
-adata.uns['leiden_nhood_enrichment']
+sq.gr.spatial_neighbors(adata)
+
+# %%
+# Then we can calculate the neighborhood enrichment score with :func:`squidpy.gr.nhood_enrichment`
+
+sq.gr.nhood_enrichment(adata, cluster_key="cluster")
+
+# %%
+# And visualize the results with :func:`squidpy.pl.nhood_enrichment`
+
+sq.pl.nhood_enrichment(adata, cluster_key="cluster")
