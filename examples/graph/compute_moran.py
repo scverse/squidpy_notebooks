3ab2e5ba5fc009d51f614ea54ca10c6d44e8e57b
Michal Klein <michal.klein@protonmail.com>
2021-05-06 20:21:21 +0200

    Use squidpy.gr.spatial_autocorr

--- examples/graph/compute_moran.py
+++ examples/graph/compute_moran.py
@@ -8,7 +8,6 @@ This example shows how to compute the Moran's I global spatial autocorrelation s
 The Moran's I global spatial autocorrelation statistics evaluates whether
 features (i.e. genes) shows a pattern that is clustered, dispersed or random
 in the tissue are under consideration.
-It wraps an implementation in the PySAL spatial analysis toolbox :cite:`pysal`.
 
 .. seealso::
 
@@ -24,14 +23,15 @@ adata = sq.datasets.visium_hne_adata()
 adata
 
 ###############################################################################
-# We can compute the Moran's I score with :func:`squidpy.gr.moran`.
+# We can compute the Moran's I score with :func:`squidpy.gr.spatial_autocorr` and ``mode = 'moran'``.
 # We first need to compute a spatial graph with :func:`squidpy.gr.spatial_neighbors`.
-# We will also subset the number of genes to evaluate
+# We will also subset the number of genes to evaluate.
 
-genes = adata[:, adata.var.highly_variable].var_names.values[0:100]
+genes = adata[:, adata.var.highly_variable].var_names.values[:100]
 sq.gr.spatial_neighbors(adata)
-sq.gr.moran(
+sq.gr.spatial_autocorr(
     adata,
+    mode="moran",
     genes=genes,
     n_perms=100,
     n_jobs=1,
@@ -41,3 +41,7 @@ adata.uns["moranI"].head(10)
 ###############################################################################
 # We can visualize some of those genes with :func:`scanpy.pl.spatial`.
 sc.pl.spatial(adata, color=["Resp18", "Tuba4a"])
+
+###############################################################################
+# We could've also passed ``mode = 'geary'`` to compute a closely related autocorrelation statistic, `Geary's C
+# <https://en.wikipedia.org/wiki/Geary%27s_C>`_. See :func:`squidpy.gr.spatial_autocorr` for more information.
