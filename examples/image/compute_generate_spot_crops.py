bfa42f7788e94ef5f026c1a548b68309172def3f
giovp <giov.pll@gmail.com>
2022-04-11 15:44:45 +0200

    modify example

--- examples/image/compute_generate_spot_crops.py
+++ examples/image/compute_generate_spot_crops.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 """
-Generate cropped images using :meth:`squidpy.im.ImageContainer.generate_spot_crops`
---------------------
+Generate cropped images from spots
+----------------------------------
+
 This example shows how to use :meth:`squidpy.im.ImageContainer.generate_spot_crops`.
 
 High-resolution tissue slides might be too large to fit in the memory.
 Therefore, we use a generator that produces cropped images from the original image container object.
 :meth:`squidpy.im.ImageContainer.generate_spot_crops` iterates over :attr:`anndata.AnnData.obsm` and extracts crops.
-Implemented for 10X spatial datasets.
+
 For Z-stacks, the specified ``library_id`` or list of ``library_id`` need to match the name of the Z-dimension.
 Always extracts 2D crops from the specified Z-dimension.
 
@@ -32,29 +33,14 @@ adata = sq.datasets.visium_hne_adata_crop()
 
 ###############################################################################
 # Next, we use :meth:`squidpy.im.ImageContainer.generate_spot_crops` to make a generator that generates cropped images.
-# The arguments supported by :meth:`squidpy.im.ImageContainer.generate_spot_crops` are the following:
-# * :class:`anndata.AnnData`, an annotated data object.
-# * spatial_key (``str``), a key in ``anndata.AnnData.obsm`` where spatial coordinates are stored.
-# * ``spot_scale``, a float, is scaling factor for the spot diameter. Larger values mean more context.
-# * library_id (``Optional``[``str``]) –
-#       *If _None_, there should only exist one entry in ``anndata.AnnData.uns`` [``'{spatial_key}'``].
-#       *If a ``str``, first search ``anndata.AnnData.obs`` [``'{library_id}'``] which contains the mapping
-#        from observations to library ids, then search ``anndata.AnnData.uns`` [``'{spatial_key}'``].
-# * ``obs_names`` (``Optional``[``Iterable``[``Any``]]) – Observations from ``anndata.AnnData.obs_names``
-#   for which to generate the crops. If _None_, all observations are used.
-# * ``as_array``, if a ``str``, yields numpy array of the specified layer.
-#   For example, ```as_array="image"``` specifies that we will crop from the "image" layer.
-#   We can specify multiple layers to obtain crops from multiple pre-processing steps.
-#       * ``as_array``, if ``True``, returns a ``dict`` object. Else, returns :class:`squidpy.im.ImageContainer`.
-# * ``squeeze``, a ``bool`` removes singleton dimensions from the results, if ```as_array = True```.
-# * ``return_obs``, a ``bool`` to yield names from ``obs_names``.
-# * kwargs (``Any``), keyword arguments for ``crop_center()``.
-# The ``gen`` can be looped on to generate the cropped images.
+# The argument ``as_array`` specify the type in which the crop is returned. If we specificy a specific
+# layer in the :class:`squidpy.im.ImageContainer` then it will return a plain :class:`numpy.ndarray`.
+# Check the documentation of the method :meth:`squidpy.im.ImageContainer.generate_spot_crops`.
 gen = img.generate_spot_crops(adata, scale=0.5, as_array="image", squeeze=True)
 
 ###############################################################################
 # When called, the ``next(gen)`` produces consecutive cropped images each time.
-# Now, we plot the cropped images using matplotlib.
+# Let's plot the cropped images using matplotlib.
 fig, axes = plt.subplots(1, 5)
 fig.set_size_inches((20, 6))
 for i in range(5):
@@ -64,42 +50,47 @@ for i in range(5):
 
 ###############################################################################
 # We will now see how the cropped images differ with change in ``spot_size``.
-# Scale=1 would crop the spot with exact coordinates.
-# To illustrate, we change the spot_size and plot the images again by looping on ``next(gen)``.
+# ``scale = 1`` would crop the spot with exact diameter size. You can crop larger area by
+# increasing the ``scale``.
+# To illustrate this, we change the spot_size and plot the images again by looping on ``next(gen)``.
 gen = img.generate_spot_crops(adata, scale=1.5, as_array="image", squeeze=True)
 fig, axes = plt.subplots(1, 5)
 fig.set_size_inches((20, 6))
 for i in range(5):
-    axes[i].set_title(f"Cropped image {i+1}")
+    axes[i].set_title(f"Cropped spot {i}")
     axes[i].axis("off")
     axes[i].imshow(next(gen))
+
 ###############################################################################
 # We can see the increase in the context with increase in the ``spot_size``.
 gen = img.generate_spot_crops(adata, spot_scale=2, as_array="image", squeeze=True)
 fig, axes = plt.subplots(1, 5)
 fig.set_size_inches((20, 6))
 for i in range(5):
-    axes[i].set_title(f"Cropped image {i+1}")
+    axes[i].set_title(f"Cropped spot {i}")
     axes[i].axis("off")
     axes[i].imshow(next(gen))
 
 ###############################################################################
 # Argument ``as_array`` also takes boolean ``True`` to return a ``dict`` where the keys are layers and
-# values are ```numpy.ndarray```.
+# values are ``numpy.ndarray``. In this case, there is only one layer: ``'image'``
 gen = img.generate_spot_crops(adata, spot_scale=0.5, as_array=True, squeeze=True)
-for _i in range(1):
-    print(next(gen))
+dic = next(gen)
+image = dic["image"]
+plt.imshow(image)
 
 ###############################################################################
 # Passing ``False`` to the argument ``as_array`` returns a :class:`squidpy.im.ImageContainer`.
-gen = img.generate_spot_crops(adata, spot_scale=1.5, as_array=False, squeeze=True)
-for _i in range(5):
-    print(next(gen))
+gen = img.generate_spot_crops(adata, spot_scale=2, as_array=False, squeeze=True)
+for _ in range(5):
+    next(gen).show(figsize=(2, 2), dpi=40)
 
 ###############################################################################
-# If ```return_obs = True```, yields a ``tuple`` (cropped image, ``obs_name``). Otherwise, yields just the crops.
-# The type of the crops depends on ```as_array``` and the number of dimensions on ``squeeze``.
-# This can be used to train the classifiers in machine learning.
-gen = img.generate_spot_crops(adata, spot_scale=2, as_array=False, squeeze=True, return_obs=True)
-for _i in range(5):
-    print(next(gen))
+# If ``return_obs = True``, yields a ``tuple`` (cropped image, ``obs_name``). Otherwise, yields just the crops.
+# The type of the crops depends on ``as_array`` and the number of dimensions on ``squeeze``.
+# Such generator could be used downstream in machine learning applications, where the class label as well
+# as the image is needed.
+gen = img.generate_spot_crops(adata, spot_scale=2, as_array="image", squeeze=True, return_obs=True)
+image, obs_name = next(gen)
+plt.imshow(image)
+plt.title(obs_name)
